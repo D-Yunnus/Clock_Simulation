7e0c50b35ed63b3cd86ec5ccf33892bf8ec3a072
DylMi <DylMi@DESKTOP-LI69P79>
2024-08-09 10:58:26 +0100

    Minor Fix.

--- PTP_Simulator.m
+++ PTP_Simulator.m
@@ -1,4 +1,4 @@
-function [x_master,x_slave] = PTP_Simulator(n,d,T,X0_master,diff_master,mu_master,diff_slave,mu_slave)
+function [x_master,x_slave] = PTP_Simulator(n,d,T,X0_master,diff_master,mu_master,X0_slave,diff_slave,mu_slave)
 
 diff=diff_master;
 X_0=X0_master;
@@ -11,7 +11,7 @@ mu=mu_slave;
 x_slave=zeros(1,n/T+1);
 
 for i=1:n/T
-    [x,~,~]=Clock_Simulator(T,1,[x_master((i-1)*T+1),y_master((i-1)*T+1),diff_slave(3)],diff,mu);
+    [x,~,~]=Clock_Simulator(T,1,[x_master((i-1)*T+1),y_master((i-1)*T+1),X0_slave(3)],diff,mu);
     x_slave((i-1)*T+1:i*T+1)=x;
 end
 
