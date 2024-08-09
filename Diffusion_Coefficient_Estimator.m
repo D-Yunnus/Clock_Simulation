228e85fb8854877d0d3de98fc4ef205cccd4c16f
DylMi <DylMi@DESKTOP-LI69P79>
2024-08-09 10:14:45 +0100

    Minor Revisions.

--- Diffusion_Coefficient_Estimator.m
+++ Diffusion_Coefficient_Estimator.m
@@ -1,4 +1,4 @@
-function diff = Diffusion_Coefficient_Estimator(AVAR_small,AVAR_large,t_small,t_large,diff_drift)
+function diff = Diffusion_Coefficient_Estimator(AVAR_1,AVAR_2,t_1,t_2,diff_drift)
 
 % The Allan variance is a function of the time interval t indicating the stability of the clock.
 % The function makes an estimate for the diffusion coefficients using a
@@ -6,11 +6,11 @@ function diff = Diffusion_Coefficient_Estimator(AVAR_small,AVAR_large,t_small,t_
 
 % For small time t_small, white noise dominates.
 
-diff_white=sqrt(AVAR_small*sqrt(t_small));
+diff_white=sqrt(abs((AVAR_2*t_1-AVAR_1*t_2)/(t_1/t_2-t_2/t_1)));
 
 % For (relatively) large time t_large, flicker noise dominates.
 
-diff_flicker=sqrt(3*AVAR_large)/sqrt(t_large*sqrt(t_large));
+diff_flicker=sqrt(abs((3*(AVAR_2-diff_white^2/t_2))/t_2));
 
 % Generate diffusion vector.
 
