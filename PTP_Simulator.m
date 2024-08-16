b9416bb0aea07541fcb8f9b94b919ea87e9f9a29
DylMi <DylMi@DESKTOP-LI69P79>
2024-08-16 15:56:12 +0100

    Current Model of Clock and Time Dissemination.
    Next stage: Add additional boundary clocks.

--- PTP_Simulator.m
+++ PTP_Simulator.m
@@ -1,51 +1,90 @@
-function x_slave=PTP_Simulator(master_time,slave_freq,slave_drift,diff_slave,mu_slave,time_interval,delta)
+function x_slave=PTP_Simulator(master_time,slave_freq,slave_drift,diff_slave,mu_slave,sync_interval,delay,packet_loss_matrix)
 
-    [t,n]=size(slave_freq);
-    N=n-1;
-    diff=diff_slave;
-    mu=mu_slave;
+% The function simulates the Precision Timing Protocol which at regular
+% intervals correct the slave clock to align with the master clock.
+% In the event that packets are loss during the communication, the slave
+% clock enters a holdover mode which continue to accumulate error until the
+% next iteration of PTP.
 
-    x_slave=zeros(1,N/t+1);
+% Defining ticks.
 
-    transition_matrix=[0.95 0.05 ; 0.95 0.05];
-    mc=dtmc(transition_matrix);
-    x0=ones(1,mc.NumStates);
-    current_state=1;
+[t,n]=size(slave_freq);
+N=n-1;
 
-    for i=2:N/t+1
+% Slave clock parameters.
+
+diff=diff_slave;
+mu=mu_slave;
+
+% Initialise slave clock time deviation.
+
+x_slave=zeros(1,N/t+1);
+
+% Gilbert-Elliot model of packet loss using a two state burst model.
+
+mc=dtmc(packet_loss_matrix);
+x0=ones(1,mc.NumStates);
+current_state=1;
+
+% Loop to iterate the slave clocks time deviation from true time at each
+% tick.
+
+for i=2:N/t+1
         
-        % Modified clock simulation.
+    % Modified clock simulation for slave clock time.
+    % the slave_freq is obtained by syncE in white rabbit.
 
-        Q=[(t)*diff(1)^2+(t^3/3)*diff(2)^2+(t^5/20)*diff(3)^2 , (t^2/2)*diff(2)^2+(t^4/8)*diff(3)^2 , (t^3/6)*diff(3)^2 ;
+    Q=[(t)*diff(1)^2+(t^3/3)*diff(2)^2+(t^5/20)*diff(3)^2 , (t^2/2)*diff(2)^2+(t^4/8)*diff(3)^2 , (t^3/6)*diff(3)^2 ;
        (t^2/2)*diff(2)^2+(t^4/8)*diff(3)^2 , (t)*diff(2)^2+(t^3/3)*diff(3)^2 , (t^2/2)*diff(3)^2 ;
        (t^3/6)*diff(3)^2 , (t^2/2)*diff(3)^2 , (t)*diff(3)^2];
 
-        noise=mvnrnd(zeros(1,3),Q,1);
+    noise=mvnrnd(zeros(1,3),Q,1);
 
-        x_slave(i)=x_slave(i-1)+t*(mu(1)+slave_freq(i-1))+(t^2/2)*(mu(2)+slave_drift)+(t^3/6)*mu(3)+noise(1);
+    x_slave(i)=x_slave(i-1)+t*(mu(1)+slave_freq(i-1))+(t^2/2)*(mu(2)+slave_drift)+(t^3/6)*mu(3)+noise(1);
         
-        % PTP 'handshake'
+    % PTP message exchange to correct slave time.
+    % Check whether to preform ptp at the given time.
+    % Otherwise clock continues to deviate from true time in holdover mode
+    % unti the next PTP cycle.
 
-        if mod(i,time_interval)==0
+    if mod(i,sync_interval)==0
 
-            t_1=master_time(i);
-            
+        % Check if packet makes the first-trip.
+
+        X=simulate(mc,1,'X0',x0);
+        current_state=X(2,current_state);
+
+        if current_state==1
+
+            % Check if packet makes the round-trip.
+
             X=simulate(mc,1,'X0',x0);
-            current_state=X(2,current_state);
+            current_state=X(2,current_state)+normrnd(0,10^-12);
 
-            t_2=x_slave(i)+delta+normrnd(0,400*10^-6)+1*(current_state-1);
-            t_3=t_2;
+            if current_state==1
 
-            X=simulate(mc,1,'X0',x0);
-            current_state=X(2,current_state);
+                % PRP timestamping.
 
-            t_4=master_time(i)+2*delta+normrnd(0,400*10^-6)+1*(current_state-1);
+                t_1=master_time(i);
+                t_2=x_slave(i)+delay+normrnd(0,10^-12);
+                t_3=t_2;
+                t_4=master_time(i)+2*delay++normrnd(0,10^-12);
 
-            one_way_flight=0.5*((t_4-t_1)-(t_3-t_2));
+                % Calculate one way time of flight.
 
-            offset=t_2-t_1-one_way_flight;
+                one_way_flight=0.5*((t_4-t_1)-(t_3-t_2));
 
-            x_slave(i)=x_slave(i)-offset;
+                % Time offset between slave and master clock.
+
+                offset=t_2-t_1-one_way_flight;
+
+                % Correct slave time.
+
+                x_slave(i)=x_slave(i)-offset;
+
+            end
         end
     end
+end
+
 end
--- SyncE_Simulator.m
+++ SyncE_Simulator.m
@@ -1,15 +1,22 @@
-function [y_master,y_slave] = SyncE_Simulator(N,t,X0_master,diff_master,mu_master,X0_slave,diff_slave,mu_slave,Adjustment_Power)
+function [x_master,y_master,y_slave] = SyncE_Simulator(N,t,X0_master,diff_master,mu_master,X0_slave,diff_slave,mu_slave,adjustment,filter_freq,packet_loss_matrix)
 
-% The simulator simulates Synchronous Ethernet which phase locks a slave
+% The function simulates Synchronous Ethernet which phase locks a slave
 % clocks frequency with the master clocks frequency.
 
 % Use Clock_Simulator.m to obtain a master clock frequency.
 
-[~,y_master,~]=Clock_Simulator(N,t,X0_master,diff_master,mu_master);
+[x_master,y_master,~]=Clock_Simulator(N,t,X0_master,diff_master,mu_master);
 
 % Initialise slave clock frequency.
 
 y_slave=zeros(1,N/t+1);
+
+% Gilbert-Elliot Model for packet loss.
+% The model uses a two-state Markov Chain to demostrate burst packet loss.
+
+mc=dtmc(packet_loss_matrix);
+x0=ones(1,mc.NumStates);
+current_state=1;
 
 % Phase Lock Loop.
 % The phase lock loop has slave frequency match master frequency for each
@@ -22,25 +29,51 @@ for i=2:N/t+1
 
     [~,y,~]=Clock_Simulator(t,t,X0_slave,diff_slave,mu_slave);
 
-    % Calculate error between master clock and slave clock frequencies.
+    % Check if packet makes the first-trip.
 
-    FreqError=y_master(i)-y(end);
+    X=simulate(mc,1,'X0',x0);
+    current_state=X(2,current_state);
 
-    % Adjust slave clock frequency to match master clock frequency.
-    % Adjust_Power is the amount of error accounted for to match the
-    % frequencies.
-    % Note: Adjust_Power=1 means y_slave=y_master exactly.
+    if current_state==1
 
-    y_slave(i)=y(end)+Adjustment_Power*FreqError;
+        % Check if packet makes the round-trip.
+        
+        X=simulate(mc,1,'X0',x0);
+        current_state=X(2,current_state);
+
+        if current_state==1
+
+
+            % Calculate error between master clock and slave clock frequencies.
     
-    % Iterate the intial frequency of the slave clock for the next
-    % iteration of the loop.
+            FreqError=y_master(i)-y(end);
 
-    X0_slave(2)=y_slave(i);
+            % Adjust slave clock frequency to match master clock frequency.
+            % Adjust_Power is the amount of error accounted for to match the
+            % frequencies.
+            % Note: Adjust_Power=1 means y_slave=y_master exactly.
+
+            y_slave(i)=y(end)+adjustment*FreqError;
+    
+            % Iterate the intial frequency of the slave clock for the next
+            % iteration of the loop.
+
+            X0_slave(2)=y_slave(i);
+
+        end
+    else
+
+        % If the packet is lost then the slave clock is in holdover mode
+        % for another iteration before syncE is performed again.
+
+        y_slave(i)=y(end);
+        X0_slave(2)=y_slave(i);
+        
+    end
 end
 
 % Filter slave noise through a low pass filter
 
-y_slave=lowpass(y_slave,FilterFreq);
+y_slave=lowpass(y_slave,filter_freq);
 
 end
