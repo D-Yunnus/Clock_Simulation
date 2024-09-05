bab050dad24c5f8b3174f6f2fbd589d02201d5b9
DylMi <DylMi@DESKTOP-LI69P79>
2024-09-05 15:17:10 +0100

    ELSTAB Implementation using a numerical method for a DLL system.

---
+++ ELSTAB_Simulator.m
@@ -0,0 +1,80 @@
+function [Slave_Time,Corrected_Freq,Corrected_Phase,Master_Time] = ELSTAB_Simulator( N , t , Master_Clock , Delay , Delay_Uncertainty , alpha , beta , Filter_Freq , Distance , Speed )
+
+% Initialise values.
+
+Master_Freq=zeros(1,N/t+1);
+Master_Err=zeros(1,N/t+1);
+Master_Phase=zeros(1,N/t+1);
+Delay_Phase=zeros(1,N/t+1);
+Delay_Phase(1)=Delay;
+Delay_Err=zeros(1,N/t+1);
+Corrected_Phase=zeros(1,N/t+1);
+Corrected_Freq=zeros(1,N/t+1);
+Master_Time=zeros(1,N/t+1);
+
+% Parameters for master clock.
+
+X0_Master=Master_Clock(1:3);
+Diff_Master=Diffusion_Coefficient_Estimator(Master_Clock(4),Master_Clock(6),Master_Clock(5),Master_Clock(7),0);
+Mu_Master=Master_Clock(8:10);
+
+for i=2:N/t+1
+
+    % Iterate the the master frequency.
+
+    [~,y,~]=Clock_Simulator(t,t,X0_Master,Diff_Master,Mu_Master);
+    Master_Freq(i)=y(end);
+
+    % Calculate phase error per second and the accumulative phase.
+
+    Master_Err(i)=0.5*(Master_Freq(i)-Master_Freq(i-1));
+    Master_Phase(i)=Master_Phase(i-1)+Master_Err(i);
+
+    % Generate delay phase.
+
+    Delay_Time=normrnd(Delay,Delay_Uncertainty);
+    Delay_Phase(i)=Master_Phase(i)+Delay_Time;
+    Delay_Err(i)=Delay_Phase(i)-Delay_Phase(i-1);
+
+    % Calculate the delay uncertainty numerically and correct the phase.
+
+    Uncertainty=alpha*(Delay_Err(i)-Master_Err(i))+beta*(Delay_Phase(i-1)-Master_Phase(i-1))-Delay;
+    Corrected_Phase(i)=Master_Phase(i)+Uncertainty;
+
+    % Correct the master phase.
+
+    Master_Phase(i)=Corrected_Phase(i);
+end
+
+% Calculate corrected frequency.
+
+for i=2:N/t+1
+    Corrected_Freq(i)=(Corrected_Phase(i)-Corrected_Phase(i-1))/t+10*10^-8;
+end
+
+% Use lowpass filter.
+
+Corrected_Freq=lowpass(Corrected_Freq,Filter_Freq);
+
+% Loop master time.
+
+diff=Diff_Master;
+mu=Mu_Master;
+
+for i=2:N/t+1
+
+    Q=[(t)*diff(1)^2+(t^3/3)*diff(2)^2+(t^5/20)*diff(3)^2 , (t^2/2)*diff(2)^2+(t^4/8)*diff(3)^2 , (t^3/6)*diff(3)^2 ;
+       (t^2/2)*diff(2)^2+(t^4/8)*diff(3)^2 , (t)*diff(2)^2+(t^3/3)*diff(3)^2 , (t^2/2)*diff(3)^2 ;
+       (t^3/6)*diff(3)^2 , (t^2/2)*diff(3)^2 , (t)*diff(3)^2];
+
+    noise=mvnrnd(zeros(1,3),Q,1);
+
+    Master_Time(i)=Master_Time(i-1)+t*(mu(1)+Master_Freq(i-1))+(t^2/2)*(mu(2)+0)+(t^3/6)*mu(3)+noise(1);
+
+end
+
+% Synchronise slave clock continuously.
+
+Slave_Time=PTP_Simulator(N,t,Master_Time,Corrected_Freq,X0_Master(3),Diff_Master,Mu_Master,2,Distance,Speed,[1,0;1,0]);
+
+end
--- Phase_Detector_Simulation.m
+++ Phase_Detector_Simulation.m
@@ -1,18 +1,26 @@
-function [Slave_Phase, Phase_Difference] = Phase_Detector_Simulation(Master_Freq,Slave_Freq)
-
-[t,n]=size(Slave_Freq);
-N=n-1;
+function [Error_Phase_Difference,Phase_Difference,Master_Phase,Slave_Phase] = Phase_Detector_Simulation(N,t,Master_Freq,Slave_Freq)
 
 Master_Phase=zeros(1,N/t+1);
 Slave_Phase=zeros(1,N/t+1);
+Error_Master_Phase=zeros(1,N/t+1);
+Error_Slave_Phase=zeros(1,N/t+1);
+
+Master_Area=0;
+Slave_Area=0;
 
 for i=2:N/t+1
-    Master_Area=+0.5*(Master_Freq(i)+Master_Freq(i-1))*t;
+
+    Error_Master_Phase(i)=0.5*(Master_Freq(i)+Master_Freq(i-1))*t;
+    Master_Area=Master_Area+Error_Master_Phase(i);
     Master_Phase(i)=Master_Area;
-    Slave_Area=+0.5*(Slave_Freq(i)+Slave_Freq(i-1))*t;
+
+    Error_Slave_Phase(i)=0.5*(Slave_Freq(i)+Slave_Freq(i-1))*t;
+    Slave_Area=Slave_Area+Error_Slave_Phase(i);
     Slave_Phase(i)=Slave_Area;
+
 end
 
 Phase_Difference=Slave_Phase-Master_Phase;
+Error_Phase_Difference=Error_Slave_Phase-Error_Master_Phase;
 
 end
